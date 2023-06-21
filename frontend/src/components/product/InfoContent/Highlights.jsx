ca72704cd20b7cfa4fc60d4e563d470b0137723e
Jacob <simerly81@gmail.com>
2023-06-20 21:22:53 -0400

    smaller font on About this Item

--- frontend/src/components/product/InfoContent/Highlights.jsx
+++ frontend/src/components/product/InfoContent/Highlights.jsx
@@ -3,7 +3,7 @@
 const Highlights = ({highlights}) => {
   return (
     <div className="flex justify-center">
-      <ul className='list-disc w-[90%]'>
+      <ul className='list-disc w-[90%] text-[14px]'>
           {highlights.map((highlight, i) => (
               <li key={'highlights_'+i}>
                 {highlight.highlight}
