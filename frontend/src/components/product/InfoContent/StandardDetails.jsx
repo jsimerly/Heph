d86111f1a8ccf7869a69c3f94df758fc9651204c
Jacob <simerly81@gmail.com>
2023-06-20 21:15:48 -0400

    update to allow breaks

--- frontend/src/components/product/InfoContent/StandardDetails.jsx
+++ frontend/src/components/product/InfoContent/StandardDetails.jsx
@@ -1,10 +1,12 @@
 
 const StandardDetails = ({desc}) => {
+  const paragraphs = desc.split('\n').filter(paragraph => paragraph.trim() !== '');
+
   return (
     <p className="px-6 pb-4 text-[16px] tracking-wide">
-      {desc.split('\n').map((paragraph, i) => {
-        return <span key={'paragraph_'+i}>{paragraph}<br/></span>
-      })}
+      {paragraphs.map((paragraph, i) => (
+        <>{paragraph}<br/></>
+      ))}
     </p>
   )
 }
